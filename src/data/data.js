e8acd6d5e1fe7d2bfb9ccbc4b972efa25bacaef1
lavender-flower <priniyaaa@gmail.com>
2024-05-16 01:57:36 +0600

    doctos page add

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+const doctors=[
+    {img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor1.jpg',
+        name:'Dr. Emily Walker',
+        department:'Pediatrician'
+    },
+
+    {img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor2.jpg',
+    name:'Dr. Olivia Bennett',
+    department:'Gynecologists'
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor3.jpg',
+    name:'Dr. Nathan Anderson',
+    department:'Cardiologists',
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor4.jpg',
+    name:'Dr. Grace Mitchell',
+    department:'Orthopedic',
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor12.jpg',
+    name:'Dr. Sussie Wolff',
+    department:'Dermatologist',
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor13.jpg',
+    name:'Dr. Hellen Low',
+    department:'Psychologist',
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor14.jpg',
+    name:'Dr. Irene Dermott',
+    department:'Dentist',
+},
+{
+    img:'https://themes.envytheme.com/mavis/wp-content/uploads/2024/04/doctor15.jpg',
+    name:'Dr. Jasmine Jordan',
+    department:'Neurologist',
+},
+
+]
